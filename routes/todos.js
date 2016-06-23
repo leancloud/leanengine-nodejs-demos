011b616390b9c6391b8176f68546c9def73d8d43
jysperm <jysperm@gmail.com>
2016-06-23 16:55:21 +0800

    处理 Class 不存在时的 101 错误

--- routes/todos.js
+++ routes/todos.js
@@ -29,6 +29,20 @@ router.get('/', function(req, res, next) {
       status: status,
       errMsg: errMsg
     });
+  }, function(err) {
+    if (err.code === 101) {
+      // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的 Todo 列表。
+      // 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
+      res.render('todos', {
+        title: 'TODO 列表',
+        user: req.currentUser,
+        todos: [],
+        status: status,
+        errMsg: errMsg
+      });
+    } else {
+      throw err;
+    }
   }).catch(next);
 });
 
