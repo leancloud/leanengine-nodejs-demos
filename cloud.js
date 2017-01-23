1be420253b693fc23d9eb7b939af3e8c107d4ebf
Cui Yiran <cuiyiran@cuiyirandeMacBook-Pro.local>
2017-01-23 15:32:55 +0800

    feat: 增加云函数超时处理的示例代码

--- cloud.js
+++ cloud.js
@@ -1,6 +1,9 @@
 var AV = require('leanengine');
 var _ = require('underscore');
 
+// 声明 Task
+var Task = AV.Object.extend('Task');
+
 /**
  * 一个简单的云代码方法
  */
@@ -24,5 +27,59 @@ AV.Cloud.beforeSave('Todo', function(req, res) {
   todo.set('tags', tags);
   res.success();
 })
+
+/**
+云函数超时示例
+https://leancloud.cn/docs/leanengine_cloudfunction_guide-node.html#超时的处理方案
+示例中写了一个没有具体业务场景的任务。
+写自己的具体业务场景下的任务时，我们建议设计成幂等任务，使其重复执行也不会有问题。
+*/
+function doTask() {
+  console.log('begin task');
+  return new Promise(function(resolve, reject) {
+    // 随机运行时长 1s ~ 20s 之间
+    var randomTime = (Math.floor(Math.random() * 20 + 1)) * 1000;
+    setTimeout(function () {
+      // 随机时长为奇数时，模拟任务失败
+      if (randomTime % 2000 === 0) {
+        resolve();
+      } else {
+        reject(new Error('some reasons for task failed'));
+      }
+    }, randomTime);
+  });
+  
+}
+
+AV.Cloud.define('doTask', function(req, res) {
+  var task = new Task();
+  var taskName = req.params.name;
+  // 存储任务队列，这里只设置了 name 字段，可以根据业务需求增加其他的字段信息
+  task.set('name', taskName);
+  // 设置状态为「处理中」
+  task.set('status', 'pending');
+  task.save().then(function (task) {
+    // 先返回任务 Id，再执行任务
+    res.success(task.id);
+    doTask().then(function() {
+      // 任务成功完成，设置状态为「成功」
+      task.set('status', 'success');
+      return task.save();
+    }).then(function(task) {
+      console.log('task succeed');
+    }).catch(function (error) {
+      // 任务失败，设置状态为「失败」
+      task.set('status', 'failure');
+      task.save().then(function(task) {
+        console.log('task failed');
+      }).catch(function(error) {
+        console.log('更新 task 失败', error);
+      });
+    });
+  }).catch(function(error) {
+    // 任务队列保存失败，返回失败
+    res.err(error);
+  });
+});
 
 module.exports = AV.Cloud;
